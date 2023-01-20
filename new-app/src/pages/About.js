1bfdeef1963fa3a5428bfd1442b509d3e093b6b0
Laksisi Chetha Gunaratne <90137011+Laksisi99@users.noreply.github.com>
2023-01-20 10:33:30 +0530

    Created react website for practice
    Created react website for practice

---
+++ new-app/src/pages/About.js
@@ -0,0 +1,39 @@
+
+const About = () => {
+  return (
+    <main className="about">
+      <div className="pg-header">
+        <div className="container">
+          <h1>About</h1>
+        </div>
+      </div>
+      <div className="container content">
+        <div className="row">
+          <div className="col-lg-4">
+            <h2>Introducing Us</h2>
+            <p>React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+            react-router-native : React Native version.
+            React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. 
+            This library comes in th</p>
+          </div>
+          <div className="col-lg-4">
+            <h2>Our Mission.</h2>
+            <p>React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+            react-router-native : React Native version.
+            React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. 
+            This library comes in th</p>
+          </div>
+          <div className="col-lg-4">
+            <h2>Our Vision</h2>
+            <p>React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+            react-router-native : React Native version.
+            React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. 
+            This library comes in th</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default About
---
+++ new-app/src/pages/Home.js
@@ -0,0 +1,31 @@
+
+const Home = () => {
+  return (
+    <main>
+      <div className="pg-header">
+        <div className="container">
+          <h1>Welcome to Sample WebSite</h1>
+        </div>
+      </div>
+      <div className="container content">
+        <p>React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+        react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+        react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+        react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+        react-router-native : React Native version.
+        </p>
+        <p>
+        Is React good for SEO? 
+        React is a JavaScript framework developed to build interactive and modular UIs. SEO is not a design goal of React, but content websites built with React can be optimized to achieve better indexing and ranking.
+        React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+        react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+        react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+        react-router-native : React Native version.React Router is a third-party library that allow components to be used as routed endpoints within an application. This library comes in three packages: react-router : For core functionality. 
+        react-router-native : React Native version.
+        </p>
+      </div>
+    </main>
+  )
+}
+
+export default Home
